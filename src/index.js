ccdc7616250bc98cc4e955dc67a47677028779d4
Parvez095 <pmohd095@gmail.com>
2023-06-24 01:24:03 +0530

    Add configuration2 files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+axios.get('https://prismatic-lokum-919d4a.netlify.app/api/cards', {
+  params: {
+    page: 2,
+    limit: 5
+  }
+})
+  .then(response => {
+    console.log(response.data);
+    ReactDOM.render(
+      <React.StrictMode>
+        <App cards={response.data} />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
+  .catch(error => {
+    console.error(error);
+  });
